604749b01c2777dfde3e736681b2736ee0ee7ac2
dineshsreekanthan <dinsree11@gmail.com>
2023-09-14 21:46:43 +0530

    commit

--- run.py
+++ run.py
@@ -4,8 +4,15 @@ import time
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=False)
     page = browser.new_page()
-    page.goto("http://playwright.dev")
+    page.goto("https://google.com/recaptcha/api2/demo")
+    page.frame_locator("iframe[name=\"a-3c12f7h2196\"]").get_by_label("I'm not a robot").click()
+    page.frame_locator("iframe[name=\"c-3c12f7h2196\"]").locator("td:nth-child(3)").first.click()
+    page.frame_locator("iframe[name=\"c-3c12f7h2196\"]").locator("tr:nth-child(2) > td:nth-child(2)").click()
+    page.frame_locator("iframe[name=\"c-3c12f7h2196\"]").locator("tr:nth-child(3) > td:nth-child(2)").click()
+    page.frame_locator("iframe[name=\"c-3c12f7h2196\"]").get_by_role("button", name="Verify").click()
+    page.get_by_role("button", name="Submit").click()
     print(page.title())
     page.screenshot(path="./img/example.png")
     page.wait_for_timeout(5000) #time.sleep(5)
     browser.close()
+    
